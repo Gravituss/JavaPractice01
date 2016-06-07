4115e2e9b003e9dac304bebb544b8bb0ba1d71c9
Gravituss <zaretskyyy@gmail.com>
2016-06-07 23:52:35 +0300

    unusual main

---
+++ J004MainUnusualParams.java
@@ -0,0 +1,11 @@
+
+public class J004MainUnusualParams {
+	
+	public static void main() {
+		System.out.println("It works!");
+	}
+	
+	public static void main(String args) {
+		System.out.println("It works!!");
+	}
+}
