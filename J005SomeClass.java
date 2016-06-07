8acba6839a364131897efe06d6b5e092a5aec4e7
Gravituss <zaretskyyy@gmail.com>
2016-06-08 01:33:28 +0300

    different tests

---
+++ J005SomeClass.java
@@ -0,0 +1,13 @@
+
+public class J005SomeClass {
+
+	public static void main(String[] args) {
+		
+		Integer[] array = null;
+		System.out.println(array);
+		int a = 00711;
+		a++;
+		int p = a;
+		System.out.println(p);
+	}
+}
