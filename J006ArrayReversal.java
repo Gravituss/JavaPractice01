2f021c7e7d755c66156c4b67d785398c17466696
Gravituss <zaretskyyy@gmail.com>
2016-06-08 23:29:30 +0300

    array reversal: adding null support

--- J006ArrayReversal.java
+++ J006ArrayReversal.java
@@ -3,29 +3,35 @@ public class J006ArrayReversal {
 
 	public static void main(String[] args) {
 		
-		int array [] = new int[]{1, 5, 77, 9, 99};
+		int array [] = null;
 		int array2 [] = new int[]{};
 		int array3 [] = new int[]{8};
-		int array4 [] = new int[]{3, 88, 55};
+		int array4 [] = new int[]{3, 88, -55};
 		
 		arrayRev(array, array2, array3, array4);
 	}
-
+	
+	
 	public static void arrayRev(int[]... a){
 		
 		for (int[] mas: a){
-			int len = mas.length;
-			printArray(mas);
-			for (int i = 0; i < len ; i++ ){
-				if (i < len/2){
-					int tmp = mas[i];
-					mas[i] = mas[len-i-1];
-					mas[len-i-1] = tmp;
+			if (mas == null){
+				System.out.println("null --> null");
+				continue;
+			} else{
+				int len = mas.length;
+				printArray(mas);
+				for (int i = 0; i < len ; i++ ){
+					if (i < len/2){
+						int tmp = mas[i];
+						mas[i] = mas[len-i-1];
+						mas[len-i-1] = tmp;
+					}
 				}
+				System.out.print(" --> ");
+				printArray(mas);
+				System.out.println("");
 			}
-			System.out.print(" --> ");
-			printArray(mas);
-			System.out.println("");
 		}
 	}
 	
