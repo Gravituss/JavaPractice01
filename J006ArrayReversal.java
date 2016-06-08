7251c4874b0cbe49e0fad3fa246276879295c2ca
Gravituss <zaretskyyy@gmail.com>
2016-06-08 23:18:27 +0300

    array reversal final commit

--- J006ArrayReversal.java
+++ J006ArrayReversal.java
@@ -3,12 +3,40 @@ public class J006ArrayReversal {
 
 	public static void main(String[] args) {
 		
+		int array [] = new int[]{1, 5, 77, 9, 99};
+		int array2 [] = new int[]{};
+		int array3 [] = new int[]{8};
+		int array4 [] = new int[]{3, 88, 55};
 		
-		
-		
-		
-		
+		arrayRev(array, array2, array3, array4);
+	}
 
+	public static void arrayRev(int[]... a){
+		
+		for (int[] mas: a){
+			int len = mas.length;
+			printArray(mas);
+			for (int i = 0; i < len ; i++ ){
+				if (i < len/2){
+					int tmp = mas[i];
+					mas[i] = mas[len-i-1];
+					mas[len-i-1] = tmp;
+				}
+			}
+			System.out.print(" --> ");
+			printArray(mas);
+			System.out.println("");
+		}
+	}
+	
+	public static void printArray(int[] arr){
+		
+		System.out.print("{");		
+		for(int i = 0; i < arr.length ; i++){
+			if (i < arr.length-1)System.out.print(arr[i] + ", ");
+			if (i == arr.length-1)System.out.print(arr[i]);
+		}
+		System.out.print("}");		
 	}
 
 }
