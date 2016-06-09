14a344d6be350da61c40a3071d85d187393b6864
Gravituss <zaretskyyy@gmail.com>
2016-06-09 16:41:59 +0300

    arraycopy

---
+++ J009ArrayCopy.java
@@ -0,0 +1,18 @@
+
+public class J009ArrayCopy {
+
+	public static void main(String[] args) {
+		
+		int [] a = {3, 5, 7};
+		int [] b = {1, 2, 3}; 
+		
+		System.arraycopy(a, 0, b, 0, 3);
+		for(int i = 0; i < a.length; i++){
+			System.out.println(a[i]);
+		}
+		System.out.println("");
+		for(int i = 0; i < b.length; i++){
+			System.out.println(b[i]);
+		}
+	}
+}
