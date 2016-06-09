265550fb4ff4713fa1dad79ee24a4048a9cfe0b0
Gravituss <zaretskyyy@gmail.com>
2016-06-09 23:27:27 +0300

    big decimals: small fix

--- J010BiggDecimals.java
+++ J010BiggDecimals.java
@@ -4,6 +4,6 @@ public class J010BiggDecimals {
 	public static void main(String[] args){
 		BigDecimal veryHugeNumber = new BigDecimal(Math.PI);
 		System.out.println(veryHugeNumber);
-		System.out.println("Only first 15 decimal points are fine"); 
+		System.out.println("Only first 15 digits are fine"); 
 	}
 }
