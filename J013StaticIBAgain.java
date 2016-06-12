96b125b4ee50363c4a49e860c027113bf1200726
Gravituss <zaretskyyy@gmail.com>
2016-06-12 19:18:01 +0300

    static init block #2

---
+++ J013StaticIBAgain.java
@@ -0,0 +1,21 @@
+
+public class J013StaticIBAgain {
+	
+	static {
+		System.out.println("Static Init Block - run once");
+	}	
+	int i;
+	
+	
+	public static void main(String[] args) {
+		System.out.println("----------");
+		J013StaticIBAgain a = new J013StaticIBAgain();
+		J013StaticIBAgain b = new J013StaticIBAgain();
+		a.i = 5;
+		b.i = 7;
+		
+		System.out.println("i = " + a.i);
+		System.out.println("i = " + b.i);
+		
+	}
+}
