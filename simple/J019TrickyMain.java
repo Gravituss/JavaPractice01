5e0192cd537100d33a93673aa3dfd42b4c0ba3e2
Gravituss <zaretskyyy@gmail.com>
2016-06-19 00:33:26 +0300

    tricky main

---
+++ simple/J019TrickyMain.java
@@ -0,0 +1,12 @@
+package simple;
+
+public class J019TrickyMain {
+	
+	public static void main(int arg){
+		System.out.println("int main");
+	}
+	
+	public static void main(String[] args) {
+		main(5);
+	}
+}
